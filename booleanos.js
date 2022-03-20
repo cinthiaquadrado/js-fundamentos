635ce9ae19fa3891222312564b5a9464275937d7
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-03-20 16:50:47 -0300

    Novos arquivos de exemplos da aula 01

---
+++ booleanos.js
@@ -0,0 +1,9 @@
+const texto1 = "Alura";
+const texto2 = "alura";
+
+//console.log(texto1 === texto2);
+
+const texto3 = "Cinthia";
+const texto4 = "cinthia";
+
+console.log(texto3 != texto4);
---
+++ manipulacao-string.js
@@ -0,0 +1,14 @@
+//const cidade = "belo horizonte";
+//const input = "Belo Horizonte";
+
+//console.log(cidade === input); // false
+
+//const cidade = "belo horizonte";
+//const input = "Belo Horizonte";
+
+//const inputMinusculo = input.toLowerCase();
+
+//console.log(cidade === inputMinusculo); // true
+
+const senha = "CinthiaQuadrado1456"
+console.log(senha.length) // 19 caracteres
