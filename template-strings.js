d4f7640facf117218e599eb88c5827c842cf49ec
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-03-21 20:50:49 -0300

    Arquivos finais de funcoes, template string, ternario e param

---
+++ template-strings.js
@@ -0,0 +1,18 @@
+// const nome = "Cinthia";
+// const idade = 2022-1992;
+// const cidadeDeNascimento = "São Paulo";
+
+// const apresentacao = `meu nome é ${nome}, tenho ${idade} anos e nasci na cidade de ${cidadeDeNascimento}`;
+
+// console.log(apresentacao);
+
+//usando template string e ternário 
+
+const cliente = "Vinicius";
+const idade = 30;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${cliente} diz: por favor, quero beber ${idade ? bebidaMaior : bebidaMenor}`;
+
+console.log(pedido);
