6679e8ecfc711357d8ec5e2b1b17eb11c086f3a0
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-03-21 21:57:20 -0300

    Últimos arquivos para atualizar.

---
+++ arrow-function.js
@@ -0,0 +1,20 @@
+function apresentar(nome) {
+    return `meu nome é ${nome}`;
+}
+
+//Arrow function
+const apresentarArrow = nome => `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;
+
+//Arrow function com mais de uma linha de instrução
+
+const somaNumerosPequenos = (numero1, numero2) => {
+    if (numero1 || numero2 > 10) {
+        return "somente números de 1 a 9"
+    } else {
+        return numero1 + numero2;
+    }
+}
+
+//hoisting> arrow function se comporta como expressão.
+
